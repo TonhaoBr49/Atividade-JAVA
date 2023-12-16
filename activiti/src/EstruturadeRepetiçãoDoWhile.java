5fae676c90be633c9a2550d8142d11f1bc9ffb69
TonhaoBr49 <150561029+TonhaoBr49@users.noreply.github.com>
2023-12-16 10:48:04 -0300

    Add files via upload
    13 Exercios realizados no dia 16/12/2023,Curso de java SENAI

---
+++ activiti/src/EstruturadeRepetiçãoDoWhile.java
@@ -0,0 +1,30 @@
+//Ruan Pablo Henrique Da Silva
+import java.util.Scanner;
+public class EstruturadeRepetiçãoDoWhile {
+    public static void main(String[] args) {
+
+        int idade;
+
+        /*
+            O loop Do-While sempre executa o bloco de repetição ao menos 1 vez pois o teste está no FINAL
+
+            No caso da estrutura de repetição WHILE o teste está no início, portanto
+            o bloco de repetição pode não ser executado dependendo da condição
+        */
+
+        Scanner reader = new Scanner(System.in);
+
+        do {
+            System.out.println("Qual a sua idade?");
+            idade = reader.nextInt();
+
+            if (idade < 0)
+                System.out.println("Por favor digite uma idade com valor positivo!");
+        }
+        while(idade < 0);
+
+        System.out.println("Faltam " + (100 - idade) + " para você chegar aos 100 anos!");
+
+        reader.close();
+    }
+}
